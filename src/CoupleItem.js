da650c917d15fc20b15787b3e5562c89e2fab1be
vitor <vitorrcosta96@gmail.com>
2021-02-14 20:39:01 -0300

    [FIX] Refatorando para novas rotas

--- src/CoupleItem.js
+++ src/CoupleItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid, Paper, Card, Button } from '@material-ui/core';
 
 var requestOptions = {
@@ -6,46 +6,55 @@ var requestOptions = {
     redirect: 'follow'
 };
 
-class CoupleItem extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            randA: 0,
-            randB: 0
-        }
-    }
+function CoupleItem() {
 
-    componentDidMount() {
+    const [rand, setRand] = useState({
+        playerA: {
+            name: "",
+            elo: ""
+        },
+        playerB: {
+            name: "",
+            elo: ""
+        },
+    });
+
+    useEffect(() => {
         fetch("http://127.0.0.1:3001/coupleRandom", requestOptions)
             .then(response => response.json())
             .then(
                 (result) => {
-                    this.setState({
-                        randA: result.randA,
-                        randB: result.randB
-                    });
+                    setRand({ ...result });
+                    console.log({ ...result })
                 },
                 (error) => {
-                    this.setState({
-                        randA: -1,
-                        randB: -1
+                    console.log(error)
+                    setRand({
+                        playerA: {
+                            name: "",
+                            elo: -1
+                        },
+                        playerB: {
+                            name: "",
+                            elo: -1
+                        },
                     });
                 }
             )
-    }
+    }, []);
 
-    render() {
-        return (
-            <Grid container justify="center" spacing={4}>
-                <Grid item xs={12} sm={12} md={6}>
-                    {this.state.randA}
-                </Grid>
-                <Grid item xs={12} sm={12} md={6}>
-                    {this.state.randB}
-                </Grid>
+    return (
+        <Grid container justify="center" spacing={4}>
+            <Grid item xs={12} sm={12} md={6}>
+                <h1>Nome: {rand.playerA.name}</h1>
+                <h2>Elo: {rand.playerA.elo}</h2>
             </Grid>
-        )
-    }
+            <Grid item xs={12} sm={12} md={6}>
+            <h1>Nome: {rand.playerB.name}</h1>
+            <h2>Elo: {rand.playerB.elo}</h2>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default CoupleItem;
