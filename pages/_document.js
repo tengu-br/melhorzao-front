a12da483a9a513b11b402001b5e1862a47b77363
vitor <vitorrcosta96@gmail.com>
2021-03-10 01:37:39 -0300

    [NEW] Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,11 +8,26 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-QSR283FTZN`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QSR283FTZN', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
